efd32d3413bbb346a6c576e614d3fc92fee71816
Gloria R <norimaqidoe@hotmail.com>
2025-03-17 23:21:07 -0600

    feat: add specific style to each stat

--- src/components/stat/Stat.jsx
+++ src/components/stat/Stat.jsx
@@ -2,7 +2,7 @@ import './Stat.scss'
 
 const Stat = ({title, hours, pastHours}) => {
   return (
-    <article className="stat" aria-labelledby={`stat-title-${title}`} aria-describedby={`stat-desc-${title}`}>
+    <article className={`stat stat__${title.replace(/\s+/g, "-")}`} aria-labelledby={`stat-title-${title}`} aria-describedby={`stat-desc-${title}`}>
       <div className="stat__content">
         <div className= "stat__header">
           <h2 className="stat__title">{title}</h2>
